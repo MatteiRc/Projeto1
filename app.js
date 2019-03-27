de47cdee449f40ce692dac5fdb0fbb6230dc2d98
Giovani  AB <gioanhesini@gmail.com>
2019-03-27 01:13:48 -0300

    [ God Mode ]

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var watsonAssistant = require('./routes/watsonAssistant');
+var watsonRouter = require('./routes/watsonAssistant');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/watsonAssistant', watsonAssistant);
+app.use('/watsonAssistant', watsonRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
