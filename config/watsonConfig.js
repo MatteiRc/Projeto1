6f6b4c53a5f8cdbafacd6a8e8c105a05b0d72786
Giovani  AB <gioanhesini@gmail.com>
2019-03-26 23:09:41 -0300

    Ye

--- config/watsonConfig.js
+++ config/watsonConfig.js
@@ -1,8 +1,8 @@
-const Assistantv1 = require('watson-developer-cloud/assistant/v1');
-const watsonConfig = new Assistantv1({
+const AssistantV1 = require('watson-developer-cloud/assistant/v1');
+const watsonAssistant = new AssistantV1({
     version: '2019-18-03',
     username: 'apikey',
     password: 'tW6LFXalQe0hdoO2Xlb7Mz1jJQxby79GN2_dkUMgrbxz',
-    url: 'https://gateway.watsonplatform.net/assistant/api/v1'
+    url: 'https://gateway.watsonplatform.net/assistant/api'
 });
-module.exports = watsonConfig;
+module.exports = watsonAssistant;
